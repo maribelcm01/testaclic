7710fcd9efcf418c42c6d1050c39f1313f6fb659
maribelcm01 <maribelcm01@outlook.com>
2020-04-25 16:26:08 -0500

    basic configuration codeigniter

---
+++ application/controllers/testalia.php
@@ -0,0 +1,8 @@
+<?php 
+	class Testalia extends CI_Controller{
+		public function index()
+	{
+		$this->load->view('inicio');
+	}
+	}
+ ?>
---
+++ application/views/inicio.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>inicio</title>
+</head>
+<body>
+	<h1>Pagina principal de Testalia</h1>
+</body>
+</html>
